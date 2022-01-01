ab6cb44bc8e8aa1f29c4ee9fabbbeb8d84e8de22
Krifty <kaitlynmariejones@gmail.com>
2022-01-01 14:30:55 -0800

    Keydown event listenr arrow keys now trigger functions

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const itemStorage = document.getElementById("game-container")
+
+
+//couch object
 const couch = document.createElement('div');
 couch.classList.add('couch');
 itemStorage.appendChild(couch);
 
+let currentObject = couch
+
+//how to move couch over
+couch.style.top = "45%";
+
+//Keydown event listener
+document.addEventListener('keydown', function(e) {
+    
+    switch (e.key) {
+        case 'ArrowLeft':
+            listenArrowLeft ();
+            break;
+        case 'ArrowRight':
+            listenArrowRight ()
+            break;  
+        case 'ArrowUp':
+            listenArrowUp ();
+            break;  
+        case 'ArrowDown':
+            listenArrowDown ();
+            break;  
+    }
+});
+            
+function listenArrowLeft () {console.log("left");}
+function listenArrowRight () {console.log("right");}
+function listenArrowUp () {console.log("up");}
+function listenArrowDown () {console.log("down");}
