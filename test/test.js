50b5610bb7b33dfd61f3f3eef4367899c50abb6b
Krifty <kaitlynmariejones@gmail.com>
2022-02-09 13:14:17 -0800

    rotating at edge does not break game

--- test/test.js
+++ test/test.js
@@ -68,10 +68,10 @@ const lineShape = new Shape(1, [1]);
 
 ///first shape currently
 function newShape(object) {
-  object.firstTile = 2
-    // startingDiv -
-    // Math.ceil(object.size / 2) -
-    // Math.ceil(object.size / 2) * gridRowLength;
+  object.firstTile = 2;
+  // startingDiv -
+  // Math.ceil(object.size / 2) -
+  // Math.ceil(object.size / 2) * gridRowLength;
   currentObject = object;
   draw(object);
 }
@@ -100,15 +100,15 @@ function undraw() {
 //keyListener
 function control(e) {
   if (e.key === "ArrowLeft") {
-    move(-1);
+    move("left", "move");
   } else if (e.key === "ArrowUp") {
-    move(-gridRowLength);
+    move("top", "move");
   } else if (e.key === "ArrowRight") {
-    move(1);
+    move("right", "move");
   } else if (e.key === "ArrowDown") {
-    move(gridRowLength);
+    move("bottom", "move");
   } else if (e.key === "r" || e.key === "R") {
-    rotaion(currentObject);
+    rotaion(currentObject, "rotate");
   } else if (e.key === " ") {
     // pause();enter
     console.log("ente");
@@ -118,29 +118,27 @@ function control(e) {
 }
 document.addEventListener("keydown", control);
 
-
-function move(num) {
+function move(edge, type) {
   let gridSize = gridDivArray.length;
-  if (num === 1) {
+  if (edge === "right") {
     //move right
-    let parametors = [-1, gridSize, gridRowLength];
-    legalMoveCheck(objectGridOccupied(), parametors, num);
-  } else if (num === -1) {
+    let parametors = [-1, gridSize, gridRowLength, 1];
+    legalMoveCheck(objectGridOccupied(), parametors, 1);
+  } else if (edge === "left") {
     //move left
-    let parametors = [0, gridSize, gridRowLength];
-    legalMoveCheck(objectGridOccupied(), parametors, num);
-  } else if (num === gridRowLength) {
-    //move up
-    let parametors = [gridSize - gridRowLength, gridSize - 1, 1];
-    legalMoveCheck(objectGridOccupied(), parametors, num);
-  } else if (num === -gridRowLength) {
-    //move down
-    let parametors = [0, gridRowLength, 1];
-    legalMoveCheck(objectGridOccupied(), parametors, num);
+    let parametors = [0, gridSize, gridRowLength, -1];
+    legalMoveCheck(objectGridOccupied(), parametors, -1);
+  } else if (edge === "bottom") {
+    let parametors = [gridSize - gridRowLength, gridSize - 1, 1, gridRowLength];
+    legalMoveCheck(objectGridOccupied(), parametors, gridRowLength);
+  } else if (edge === "top") {
+    let parametors = [0, gridRowLength, 1, -gridRowLength];
+    legalMoveCheck(objectGridOccupied(), parametors, -gridRowLength);
   } else {
     console.error("unexpected movement calculation");
   }
-  function legalMoveCheck(indexAdjust, para, num) {
+
+  function legalMoveCheck(indexAdjust, para) {
     let wallHitCheck = false;
     indexAdjust.some((value) => {
       for (let i = para[0]; i < para[1]; i = i + para[2]) {
@@ -151,49 +149,156 @@ function move(num) {
     });
     if (!wallHitCheck) {
       undraw(currentObject);
-      currentObject.firstTile = currentObject.firstTile + num;
+      drawType(type, para[3]);
       draw(currentObject);
+    }
+  }
+
+  function drawType(type, num) {
+    if (type === "move") {
+      currentObject.firstTile = currentObject.firstTile + num;
+    } else if (type === "rotate") {
+      currentObject = tempObject;
+    } else {
+      console.error("drawType unknown");
     }
   }
 }
 
 function rotaion(e) {
-  console.log("r");
-  const displayLayout = [];
-  if (!(e.size * e.size == e.layout.length)) {
-    console.error("shape size and length do not match. Can not roatate");
-  } else {
-    for (let i = 0; i < e.size; i++) {
-      for (let j = e.size - 1; j >= 0; j--) {
-        displayLayout.push(e.layout[e.size * j + i]);
-      }
+  const layoutHolder = [];
+  let gridMaxIndex = gridDivArray.length - 1;
+
+  for (let i = 0; i < e.size; i++) {
+    for (let j = e.size - 1; j >= 0; j--) {
+      layoutHolder.push(e.layout[e.size * j + i]);
     }
-    undraw(e);
-    e.layout = displayLayout;
-    draw(e);
+  }
+
+  const tempObject = Object.create(currentObject);
+  tempObject.layout = layoutHolder;
+  let potentialObjectGrid = objectGridOccupied(tempObject);
+  let currentObjectGrid = objectGridOccupied(currentObject)
+
+  console.log(potentialObjectGrid);
+  //console.log(-gridRowLength);
+  //console.log(gridMaxIndex);
+  let edge = true;
+  let leftEdgeDivArry = []
+  for (let i = 0; i < gridMaxIndex; i = i + gridRowLength) {
+    leftEdgeDivArry.push(i)
+  }
+  let rightEdgeDivArry =[]
+  let rightCornerDiv = gridRowLength -1
+  for (let i = rightCornerDiv; i <= gridMaxIndex; i = i + gridRowLength) {
+    rightEdgeDivArry.push(i)
+   }
+  let objectOnLeftEdge =currentObjectGrid.filter(div => leftEdgeDivArry.includes(div));
+  let objectCrossLeftEdge = potentialObjectGrid.filter(div => rightEdgeDivArry.includes(div));
+  if (objectOnLeftEdge.length > 0) {
+    if (objectCrossLeftEdge.length > 0) {
+      console.log("illegal move bottom");
+      edge = false;
+    }
+  }
+  let objectOnRightEdge =currentObjectGrid.filter(div => rightEdgeDivArry.includes(div));
+  let objectCrossrightEdge = potentialObjectGrid.filter(div => leftEdgeDivArry.includes(div));
+  if (objectOnRightEdge.length > 0) {
+    if (objectCrossrightEdge.length > 0) {
+      console.log("illegal move bottom");
+      edge = false;
+    }
+  }
+ 
+  potentialObjectGrid.some((gridIndex) => {
+    if (gridIndex > gridMaxIndex) {
+      console.log("illegal move bottom");
+      edge = false;
+    } else if (gridIndex < 0) {
+      console.log("illegal move top");
+      edge = false;
+    } 
+  });
+  
+  
+  if (edge == true) {
+    undraw();
+    currentObject = tempObject;
+    draw();
+  }
+
+  //objectGrid.some
+
+  //if (edge === "bottom") {
+  //  let parametors = [gridSize - gridRowLength, gridSize - 1, 1, gridRowLength];
+  //  legalMoveCheck(objectGridOccupied(), parametors, gridRowLength);
+  //}
+
+  //function legalMoveCheck(indexAdjust, para, num) {
+  //  let wallHitCheck = false;
+  //  indexAdjust.some((value) => {
+  //    for (let i = para[0]; i < para[1]; i = i + para[2]) {
+  //      if (i === value) {
+  //        wallHitCheck = true;
+  //      }
+  //    }
+  //  });
+  //  if (!wallHitCheck) {
+  //    console.log("legal");
+
+  //  } else {console.log("not legal");}
+  //}
+
+  //console.log( tempObject.layout);
+  //console.log( currentObject.layout);
+}
+
+function drawType(type) {
+  if (type === "move") {
+    currentObject.firstTile = currentObject.firstTile + num;
+  } else if (type === "rotate") {
+    currentObject = tempObject;
+  } else {
+    console.error("drawType unknown");
   }
 }
+
 /////////////////////////////////////////////////////
 /////////////////where object is in grid
 ////////////////////////
 /////////////////////////////////////////////////////////////
-function objectGridOccupied() {
+function objectGridOccupied(object = currentObject) {
   let result = [];
-  currentObject.layout.forEach((value, index) => {
-    if (value === 0) {
-    } else if (value === 1) {
-      let gridRowAdjust =
-        Math.round((index - 1) / currentObject.size) * gridRowLength;
+  if (!(object.size * object.size == object.layout.length)) {
+    console.error("shape size and length do not match. Can not roatate");
+  } else {
+    object.layout.forEach((value, index) => {
+      if (value === 0) {
+      } else if (value === 1) {
+        let gridRowAdjust =
+          Math.round((index - 1) / object.size) * gridRowLength;
         // console.log("gridRowAdjust "+ gridRowAdjust);
-      let shapeRowAdjust =
-        Math.floor(index / currentObject.size) * currentObject.size;
+        let shapeRowAdjust = Math.floor(index / object.size) * object.size;
         // console.log(shapeRowAdjust +shapeRowAdjust);
-      let indexAdjust =
-        gridRowAdjust + index - shapeRowAdjust + currentObject.firstTile;
-      result.push(indexAdjust);
-    } else {
-      console.error("layout unexpected - objectGridOccupied");
-    }
-  });
-  return result;
+        let indexAdjust =
+          gridRowAdjust + index - shapeRowAdjust + object.firstTile;
+        result.push(indexAdjust);
+      } else {
+        console.error("layout unexpected - objectGridOccupied");
+      }
+    });
+    return result;
+  }
+}
+
+//////////////////////////////////
+///////////////////////////////////
+////////////////////////////////////
+
+function objectLayoutCheck() {
+  const layoutHolder = [];
+  if (!(e.size * e.size == e.layout.length)) {
+    console.error("shape size and length do not match. Can not roatate");
+  } else {
+  }
 }
