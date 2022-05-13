046004466ff09c228a8413562dbcbc1eb25f32fe
Krizzy <kaitlynmariejones@gmail.com>
2022-05-12 17:41:06 -0700

    new Brancg racing where gonna work on racing game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
 import "./style.css";
 import moving from './modules/moving';
+import racing from './modules/racing';
 
-moving();
+
+//moving();
+racing();
---
+++ src/modules/racing.js
@@ -0,0 +1,3 @@
+function racing () {}
+
+module.exports = racing;
